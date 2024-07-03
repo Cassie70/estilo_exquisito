ed21d0a9b420ea212cfe79d53cca6ceba47a9a18
cassie70 <yael07mora@gmail.com>
2024-07-02 22:26:15 -0600

    arreglado best sellers

--- backend/controllers/best_sellers.js
+++ backend/controllers/best_sellers.js
@@ -7,9 +7,8 @@ export class BestSellersController{
 
     bestSellers = async (req, res) => {
         try {
-            
-            const usuario = await this.detalleVentaModelo.bestSellers();
-            
+            const bestSellers = await this.detalleVentaModelo.bestSellers();
+            res.json(bestSellers);
         } catch (error) {
             console.log(error)
             res.status(500).json({ error: 'No se pudo consultar la base de datos de Detalle Venta' });
